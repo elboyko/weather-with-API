dc881e035ea97ab26164d9c0509eafd26faff742
elboyko <boyko.lena@gmail.com>
2024-01-08 21:33:37 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -4,17 +4,70 @@ const form = document.querySelector('#form');
 const input = document.querySelector('.form__input');
 
 form.onsubmit = submitHandler;
-function submitHandler(e) {
+async function submitHandler(e) {
    e.preventDefault();
    if (input.value.trim() === '') {
       console.log('Enter City name');
    } else {
       console.log(input.value.trim());
    }
-   getGeo(input.value.trim())
+   const cityInfo = await getGeo(input.value.trim());
+   // console.log(cityInfo);
+   // console.log(cityInfo[0]['lat']);
+   // console.log(cityInfo[0]['lon']);//для этого из данных нужно извлечь lat и lon
+   const weatherInfo = await getWeather(cityInfo[0]['lat'], cityInfo[0]['lon']);
+   console.log(weatherInfo);
+   console.log(weatherInfo.name);//выводим название города
+   console.log(weatherInfo.main.temp);//выводим температуру
+   console.log(weatherInfo.main.humidity);//выводим влажность
+   console.log(weatherInfo.wind.speed);//выводим скорость ветра
+   console.log(weatherInfo.weather[0]['main']);//выводим картинку погоды
+
+
+   const weatherData = {
+      name: weatherInfo.name,
+      temp: weatherInfo.main.temp,
+      humidity: weatherInfo.main.humidity,
+      speed: weatherInfo.wind.speed,
+      main: weatherInfo.weather[0]['main']
+   };
+   renderWeatherData(weatherData);
+
 }
 
 //функция  поиска города
-function getGeo(name) {
-   const getUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${API_key}`
+async function getGeo(name) {
+   const getUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${API_key}`;
+
+   const response = await fetch(getUrl);
+   const data = await response.json();
+
+   return data;
+}
+//нужно сделать запрос на получение погоды
+//для этого из данных нужно извлечь lat и lon
+
+async function getWeather(lat, lon) {
+   const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${API_key}`;
+   const response = await fetch(weatherUrl);
+   const data = await response.json();
+
+   return data;
+
+}
+//теперь напишем функцию, которая будет принимать все эти значения и отображать их на странице
+
+//компануем все эти данные в виде объекта, строка 27
+
+function renderWeatherData(data) {
+   const temp = document.querySelector('.weather__temp');
+   const city = document.querySelector('.weather__city');
+   const humidity = document.querySelector('#humidity');
+   const speed = document.querySelector('#speed');
+   temp.innerText = data.temp;
+   city.innerText = data.name;
+   humidity.innerText = data.humidity;
+   speed.innerText = data.speed;
+
+
 }
